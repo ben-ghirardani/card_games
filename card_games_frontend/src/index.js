3d1422785a2b8503e02ea59c4756c209b8ae937d
Ben <pretty.fiendish@gmail.com>
2017-11-06 22:17:38 +0000

    initial setup of links and folders

--- card_games_frontend/src/index.js
+++ card_games_frontend/src/index.js
@@ -6,3 +6,23 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+// import {HashRouter, Route, Switch} from 'react-router-dom';
+
+// ReactDOM.render(
+//     <HashRouter> 
+//     <Switch>
+//         <Route exact path='/' component={App} />
+//         <Route exact path='/snap' component={Snap} />
+//     </Switch>
+//   </HashRouter>  
+//     , document.getElementById('root'));
+
+// registerServiceWorker();
