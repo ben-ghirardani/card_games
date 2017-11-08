271c539a97b8134f888b2ade5d40fbce3bc21a3e
Ben <pretty.fiendish@gmail.com>
2017-11-08 19:58:16 +0000

    setup for shuffle algorithm

---
+++ card_games_frontend/src/Component/Snap.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Snap extends React.Component{
+
+    
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
+
+}
+
+export default Snap;
