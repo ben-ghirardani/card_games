2f202078a57f97ea90889ade51e18b89382557e0
Ben <pretty.fiendish@gmail.com>
2017-11-14 20:13:23 +0000

    rearranged deck of cards in app state to increase readability, no functional change

--- card_games_frontend/src/index.js
+++ card_games_frontend/src/index.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Container/App';
+import Snap from './Component/Snap';
 import registerServiceWorker from './registerServiceWorker';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    ReactDOM.render(
+            <HashRouter> 
+            <Switch>
+                <Route exact path='/' component={App} />
+                <Route exact path='/snap' component={Snap} />
+            </Switch>
+          </HashRouter>  
+            , document.getElementById('root'));
+
 registerServiceWorker();
-
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-// import {HashRouter, Route, Switch} from 'react-router-dom';
-
-// ReactDOM.render(
-//     <HashRouter> 
-//     <Switch>
-//         <Route exact path='/' component={App} />
-//         <Route exact path='/snap' component={Snap} />
-//     </Switch>
-//   </HashRouter>  
-//     , document.getElementById('root'));
-
-// registerServiceWorker();
