1ebc92e0af7f8d4804754b8065013eca27f376bd
Ben <pretty.fiendish@gmail.com>
2017-11-15 13:42:14 +0000

    passed deck and shuffle method to Snap from App

--- card_games_frontend/src/Component/Snap.js
+++ card_games_frontend/src/Component/Snap.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class Snap extends React.Component{
 
-    
+    constructor(props){
+        super(props);
+        this.state = {
+            // add in the empty player deck and the empty computer deck. Use a method to take the deck from App passed 
+            // down in props to player and comp. deck. Use shuffle method from props. 
+        }
+    }
+
     render(){
         return(
             <div>
--- card_games_frontend/src/Container/App.js
+++ card_games_frontend/src/Container/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import Snap from '../Component/Snap';
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-        deck_of_cards: [
+        deckOfCards: [
             { suit: "Clubs", name: "Ace", value: 1 }, { suit: "Clubs", name: "Two", value: 2 }, { suit: "Clubs", name: "Three", value: 3 }, { suit: "Clubs", name: "Four", value: 4 }, { suit: "Clubs", name: "Five", value: 5 }, { suit: "Clubs", name: "Six", value: 6 }, { suit: "Clubs", name: "Seven", value: 7 }, { suit: "Clubs", name: "Eight", value: 8 }, { suit: "Clubs", name: "Nine", value: 9 }, { suit: "Clubs", name: "Ten", value: 10 }, { suit: "Clubs", name: "Jack", value: 11 }, { suit: "Clubs", name: "Queen", value: 12 }, { suit: "Clubs", name: "King", value: 13},
             { suit: "Diamonds", name: "Ace", value: 1}, { suit: "Diamonds", name: "Two", value: 2 }, { suit: "Diamonds", name: "Three", value: 3 }, { suit: "Diamonds", name: "Four", value: 4 }, { suit: "Diamonds", name: "Five", value: 5 }, { suit: "Diamonds", name: "Six", value: 6 }, { suit: "Diamonds", name: "Seven", value: 7 }, { suit: "Diamonds", name: "Eight", value: 8 }, { suit: "Diamonds", name: "Nine", value: 9 }, { suit: "Diamonds", name: "Ten", value: 10 }, { suit: "Diamonds", name: "Jack", value: 11 }, { suit: "Diamonds", name: "Queen", value: 12 }, { suit: "Diamonds", name: "King", value: 13 },
             { suit: "Spades", name: "Ace", value: 1 }, { suit: "Spades", name: "Two", value: 2 }, { suit: "Spades", name: "Three", value: 3 }, { suit: "Spades", name: "Four", value: 4 }, { suit: "Spades", name: "Five", value: 5 }, { suit: "Spades", name: "Six", value: 6 }, { suit: "Spades", name: "Seven", value: 7 }, { suit: "Spades", name: "Eight", value: 8 }, { suit: "Spades", name: "Nine", value: 9 }, { suit: "Spades", name: "Ten", value: 10 }, { suit: "Spades", name: "Jack", value: 11 }, { suit: "Spades", name: "Queen", value: 12 }, { suit: "Spades", name: "King", value: 13 },
@@ -26,13 +25,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Snap className="snap" shuffleDeck={this.shuffleDeck.bind(this)} deckOfCards={this.state.deckOfCards} />
       </div>
     );
   }
